e6e1e8f4c4e52d361d45e64e348fe0a96cc1c0a0
brightboost <info@brightboost.nl>
2022-07-13 09:28:51 +0200

    try with resources

--- src/main/java/day4/Exceptional.java
+++ src/main/java/day4/Exceptional.java
@@ -1,12 +1,25 @@
 package day4;
 
+import day1.Car;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Exceptional {
     public static void main(String[] args) {
         read("./blabla.txt");
+        try {
+            doSomething();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void doSomething() throws SQLException {
+        throw new SQLException("Oh nee een SQL fout");
     }
 
     public static void read(String fileName) {
@@ -15,8 +28,24 @@ public class Exceptional {
             int i;
             while ((i = fr.read()) != -1) {
                 System.out.print((char) i);
+                // met deze regel aan kom je in IO exception block
+                // fr.close();
             }
             fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Oh nee! Ik kan m niet vinden");
+        } catch (IOException e) {
+            System.out.println("Oh nee! Iets ging mis. Do something!");
+        }
+
+        // try with resources
+        try(FileReader fr = new FileReader(fileName)) {
+            int i;
+            while ((i = fr.read()) != -1) {
+                System.out.print((char) i);
+                // met deze regel aan kom je in IO exception block
+                // fr.close();
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Oh nee! Ik kan m niet vinden");
         } catch (IOException e) {
