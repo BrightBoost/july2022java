c1108b59cbcbca6b3cc7a77f10e86919109744f9
brightboost <info@brightboost.nl>
2022-07-15 12:15:21 +0200

    more stream api

--- src/main/java/day3b/StreamVoorbeeld.java
+++ src/main/java/day3b/StreamVoorbeeld.java
@@ -1,6 +1,7 @@
 package day3b;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -22,6 +23,25 @@ public class StreamVoorbeeld {
 
         Arrays.stream(namen).map(s -> s.length()).forEach(s -> System.out.println(s));
 
+
+        List<Integer> integerList = List.of(0, 1, 3, 54, 1, 34, 56, 7);
+        integerList.stream().forEach(i -> System.out.println(i));
+        System.out.println("****");
+        integerList.stream()
+                .sorted()
+                .distinct()
+                .forEach(i -> System.out.println(i));
+        System.out.println("****");
+        integerList.stream()
+                .map(i -> i * 3)
+                .filter(i -> i % 2 == 0)
+                .forEach(i -> System.out.println(i));
+        System.out.println("****");
+        boolean match = integerList.stream()
+                .map(i -> i * 3)
+                .filter(i -> i % 2 == 0)
+                .allMatch(i -> i > 0);
+        System.out.println(match);
 
     }
 }
